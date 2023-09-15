2192b28906db7df18515116cf6168eb88ac6a6e9
Thiago Santos <thiago.santos@smartnx.com>
2023-09-14 21:12:34 -0300

    feat: Mudança na tela de cadastro.

--- frontend/src/components/auth/users/register.jsx
+++ frontend/src/components/auth/users/register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import authServices from "../../../services/userServices";
 
 const Register = () => {
@@ -11,32 +11,96 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const response = await authServices.registerUser(email, password, fullname);
-        console.log(response);
-        if(response.status == 201){
+        if (response.status == 201) {
             alert(response.message);
             return navigate("/");
         }
     }
     return (
-        <div className="">
-            <div className="">
-                <div className="">
-                    <h2>Cadastro de Usuário</h2>
-                    <form onSubmit={handleSubmit}>
-                        <div className="">
-                            <input value={fullname} onChange={(e) => setName(e.target.value)} type="text" placeholder="Nome Completo" />
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <Link to="/" className="text-sky-500 flex">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                </svg>
+                Voltar
+            </Link>    
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    className="mx-auto h-10 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="Finance Pro"
+                />
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Cadastro de Usuário
+                </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Nome
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                value={fullname} 
+                                onChange={(e) => setName(e.target.value)}
+                                id="fullname"
+                                name="fullname"
+                                type="text"
+                                autoComplete="text"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
                         </div>
-                        <div className="">
-                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Email
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                value={email} 
+                                onChange={(e) => setEmail(e.target.value)}
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
                         </div>
-                        <div className="">
-                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Senha" />
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                Password
+                            </label>
                         </div>
-                        <div className="">
-                            <input type="submit" value="Cadastrar-se"/>
+                        <div className="mt-2">
+                            <input
+                                value={password} 
+                                onChange={(e) => setPassword(e.target.value)}
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
                         </div>
-                    </form>
-                </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Cadastre-se
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     )
