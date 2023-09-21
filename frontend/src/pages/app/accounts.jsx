ad5b620f70923b1929f70bb62a7a3c5825915bee
Thiago Santos <thiago.santos@smartnx.com>
2023-09-21 18:26:51 -0300

    feat: Aprimoranddo a tela de contas.

--- frontend/src/pages/app/accounts.jsx
+++ frontend/src/pages/app/accounts.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 import Navbar from "./navbar";
-import Index from "../../components/account/formAccount";
+import FormAccount from "../../components/account/formAccount";
 
 const Accounts = () => {
     return (
         <>
             <Navbar/>
-            <div className="p-4 sm:ml-64 h-full bg-gray-900">
+            <div className="p-4 sm:ml-64 h-screen bg-gray-900">
                 <div className=" mt-14">
-                    <Index />
+                    <FormAccount />
                 </div>
             </div>
         </>
