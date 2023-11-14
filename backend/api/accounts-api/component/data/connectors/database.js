cbabc811ca735bd1a10a3b0f22511ff1b5430d54
Thiago Santos <thiago.santos@smartnx.com>
2023-11-14 12:47:28 -0300

    refactor: Correção da conexão com o banco.

--- backend/api/accounts-api/component/data/connectors/database.js
+++ backend/api/accounts-api/component/data/connectors/database.js
@@ -1,15 +1,16 @@
 const { Pool } = require('pg');
 class Database {
     async configureConnection() {
-        const databaseConnection = new Pool({
-            user: process.env.POSTGRES_USER || 'postgres',
-            host: process.env.POSTGRES_HOST || 'localhost',
-            port: Number(process.env.POSTGRES_PORT) || 5432,
-            database: process.env.POSTGRES_DATABASE || 'login_node',
-            password: process.env.POSTGRES_PASS || '123456',
-        });
-
-        return databaseConnection;
+        if(!global.databaseConnection) {
+            global.databaseConnection = new Pool({
+                user: process.env.POSTGRES_USER || 'postgres',
+                host: process.env.POSTGRES_HOST || 'localhost',
+                port: Number(process.env.POSTGRES_PORT) || 5432,
+                database: process.env.POSTGRES_DATABASE || 'login_node',
+                password: process.env.POSTGRES_PASS || '123456',
+            });
+        }
+        return global.databaseConnection;
     }
 
     async generateConnection() {
