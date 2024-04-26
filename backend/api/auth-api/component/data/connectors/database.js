d9bd61f81f9c07d656d4479d6b0c39583de09491
Thiago Santos <thiago.santos@smartnx.com>
2024-04-26 15:02:28 -0300

    feat(FP-48): Adição da verificação do banco na rota padrão das apis

--- backend/api/auth-api/component/data/connectors/database.js
+++ backend/api/auth-api/component/data/connectors/database.js
@@ -3,10 +3,10 @@ class Database {
     async configureConnection() {
         if(!global.databaseConnection) {
             global.databaseConnection = new Pool({
-                user: process.env.POSTGRES_USER || 'postgres',
-                host: process.env.POSTGRES_HOST || 'localhost',
-                database: process.env.POSTGRES_DATABASE || 'login_node',
-                password: process.env.POSTGRES_PASS || '123456',
+                user: process.env.POSTGRES_USER,
+                host: process.env.POSTGRES_HOST,
+                database: process.env.POSTGRES_DATABASE,
+                password: process.env.POSTGRES_PASS,
                 ssl: {rejectUnauthorized: false}
             });
         }
