3f72252a7b76409473f71276895effe0ce565aae
Thiago de Oliveira Santos <65147035+Thiago-cog@users.noreply.github.com>
2024-04-26 09:46:02 -0300

    Update database.js

--- backend/api/auth-api/component/data/connectors/database.js
+++ backend/api/auth-api/component/data/connectors/database.js
@@ -5,9 +5,9 @@ class Database {
             global.databaseConnection = new Pool({
                 user: process.env.POSTGRES_USER || 'postgres',
                 host: process.env.POSTGRES_HOST || 'localhost',
-                port: Number(process.env.POSTGRES_PORT) || 5432,
                 database: process.env.POSTGRES_DATABASE || 'login_node',
                 password: process.env.POSTGRES_PASS || '123456',
+                ssl: {rejectUnauthorized: false}
             });
         }
         return global.databaseConnection;
