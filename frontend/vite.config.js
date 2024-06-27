1bee3495f787db06d821f79ae01ecebc2f25de65
Thiago Santos <thiago.santos@smartnx.com>
2024-06-26 22:55:47 -0300

    feat: teste do pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Finance Pro',
+        short_name: 'F.Pro',
+        description: 'Descrição do aplicativo',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: './src/assets/2-removebg-preview.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ],
+      },
+      devOptions: {
+        enabled: true,
+        /* other options */
+      },
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/your-cdn-url\//,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'your-cache-name',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 30 // 30 dias
+              }
+            }
+          }
+        ]
+      }
+    })
+  ],
   server: {
     host: true,
     strictPort: true,
